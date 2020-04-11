666f874e40c1d831fe3aa32dd7f854a46347fc61
Julien Vu <vu.julien@gmail.com>
2020-04-11 20:04:01 +0200

    fix connection

--- tunneldns/https_upstream.go
+++ tunneldns/https_upstream.go
@@ -39,6 +39,7 @@ func NewUpstreamHTTPS(endpoint string) (Upstream, error) {
 		TLSClientConfig:    tls,
 		DisableCompression: true,
 		MaxIdleConns:       1,
+                MaxConnsPerHost:    5,
 		Proxy:              http.ProxyFromEnvironment,
 	}
 	http2.ConfigureTransport(transport)
